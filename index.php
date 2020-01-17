a90d121f87ab8505e500911bcf5ea3bb8d34972c
Manzola Caniago <48481265+manzolacaniago13@users.noreply.github.com>
2020-01-18 03:02:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -69,17 +69,23 @@
           </form>
 
             <?php
-            $host = "padevappserver.database.windows.net";
-            $user = "manzolacaniago";
-            $pass = "P1234566a";
-            $db = "submissazure";
+                // PHP Data Objects(PDO) Sample Code:
+                try {
+                    $conn = new PDO("sqlsrv:server = tcp:padevappserver.database.windows.net,1433; Database = padevcdb", "manzolacaniago", "{your_password_here}");
+                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                }
+                catch (PDOException $e) {
+                    print("Error connecting to SQL Server.");
+                    die(print_r($e));
+                }
 
-            try {
-                $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch (Exception $e) {
-                echo "Failed: " . $e;
-            }
+                // SQL Server Extension Sample Code:
+                $connectionInfo = array("UID" => "manzolacaniago", "pwd" => "{your_password_here}", "Database" => "padevcdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+                $serverName = "tcp:padevappserver.database.windows.net,1433";
+                $conn = sqlsrv_connect($serverName, $connectionInfo);
+            ?>
+        
+        <?php
 
             if (isset($_POST['submit'])) {
                 try {
