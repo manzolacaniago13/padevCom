5a06069f1b855acc89a6bc5d361ada593f54208a
Manzola Caniago <48481265+manzolacaniago13@users.noreply.github.com>
2020-01-18 02:30:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
               $db = "submissazure";
 
               try {
-                  $conn = new PDO("sqlsrv:server = $host; Database = $db, $user, $pass");
+                  $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
                   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
               } catch (Exception $e) {
                   echo "Failed: " . $e;
