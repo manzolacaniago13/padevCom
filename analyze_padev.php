1bbdb105b16e2ca9dde9d98bbb5287c68e17f5ec
Manzola Caniago <48481265+manzolacaniago13@users.noreply.github.com>
2020-01-20 22:21:19 +0700

    Update analyze_padev.php

--- analyze_padev.php
+++ analyze_padev.php
@@ -45,6 +45,22 @@ $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
     <link href="starter-template.css" rel="stylesheet">
   </head>
 <body>
+
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+               <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                      
+                      <a class="nav-link" href="https://padevcwebapp.azurewebsites.net/">Home</a>
+                  </li>
+                  <li class="nav-item active">
+                    <a class="nav-link" href="https://padevcwebapp.azurewebsites.net/analyze_padev.php">Analyze<span class="sr-only">(current)</span></a>
+                </li>
+          </div>
+      </nav>
 	<main role="main" class="container">
     		<div class="starter-template"> <br>
         		<h1>Image Analyzer</h1>
