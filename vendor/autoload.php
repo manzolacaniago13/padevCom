25d1960e3c86263aca9c3287d0c2f5f8eb955da7
Manzola Caniago <48481265+manzolacaniago13@users.noreply.github.com>
2020-01-21 00:25:32 +0700

    Update autoload.php

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit9b8040d9b55d6335959d320303f639a3::getLoader();
+return ComposerAutoloaderInitb6d34fd6e7e35fd863b35eeb2f32e185::getLoader();
